02d03535d9bafd91edadcecfc438404bf2fa9d6a
bamboo5 <loic@cyberpunk.bike>
2015-06-01 22:37:13 +0200

    First few lines of the spider added in. Not much though.

--- scraper/scraper/spiders/bike_components.py
+++ scraper/scraper/spiders/bike_components.py
@@ -1,15 +1,21 @@
-import scrapy
+from scrapy import Spider
 
 
-class BikeComponents(scrapy.Spider):
-    name = "dmoz"
-    allowed_domains = ["dmoz.org"]
-    start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
-    ]
+class BikeComponents(Spider):
+    name = 'bike_components'
+    allowed_domains = ['bike-components.de']
+    start_urls = ['https://www.bike-components.de/advanced_search_result.php?keywords=fulcrum']
+
+    deals_list_xpath = '//li[@dealid]'
+    item_fields = {
+        'title': './/span[@itemscope]/meta[@itemprop="name"]/@content',
+        'link': './/a/@href',
+        'location': './/a/div[@class="deal-details"]/p[@class="location"]/text()',
+        'original_price': './/a/div[@class="deal-prices"]/div[@class="deal-strikethrough-price"]/div[@class="strikethrough-wrapper"]/text()',
+        'price': './/a/div[@class="deal-prices"]/div[@class="deal-price"]/text()',
+        'end_date': './/span[@itemscope]/meta[@itemprop="availabilityEnds"]/@content'
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
+        filename = response.url.split('/')[-2]
         with open(filename, 'wb') as f:
             f.write(response.body)
