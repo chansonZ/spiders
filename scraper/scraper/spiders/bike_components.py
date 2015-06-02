4e40aea4477d90327cac56da54022d9c2d5e7da4
bamboo5 <loic@cyberpunk.bike>
2015-06-02 22:07:34 +0200

    Added the scraper script.

--- scraper/scraper/spiders/bike_components.py
+++ scraper/scraper/spiders/bike_components.py
@@ -2,18 +2,10 @@ from scrapy import Spider
 
 
 class BikeComponents(Spider):
-    name = 'bike_components'
+    name = 'bike-components.de'
+
     allowed_domains = ['bike-components.de']
     start_urls = ['https://www.bike-components.de/advanced_search_result.php?keywords=fulcrum']
-
-    deals_list_xpath = '//li[@dealid]'
-    item_fields = {
-        'title': './/span[@itemscope]/meta[@itemprop="name"]/@content',
-        'link': './/a/@href',
-        'location': './/a/div[@class="deal-details"]/p[@class="location"]/text()',
-        'original_price': './/a/div[@class="deal-prices"]/div[@class="deal-strikethrough-price"]/div[@class="strikethrough-wrapper"]/text()',
-        'price': './/a/div[@class="deal-prices"]/div[@class="deal-price"]/text()',
-        'end_date': './/span[@itemscope]/meta[@itemprop="availabilityEnds"]/@content'
 
     def parse(self, response):
         filename = response.url.split('/')[-2]
